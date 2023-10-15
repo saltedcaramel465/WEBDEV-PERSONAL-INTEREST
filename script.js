24979fe4bab07fa65dc4f0ad996fdb72d0158588
rchellee <144767223+rchellee@users.noreply.github.com>
2023-10-15 14:13:18 +0800

    updated all files
    change the header

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-let sec = document.querySelectorAll('section');
-let links = document.querySelectorAll('nav a');
-window.onscroll = () =>{
-    sec.forEach(section => {
-        let top = window.scrollY;
-        let offset = section.offsetTop - 60;
-        let height = section.offsetHeight;
-        let id = section.getAttribute('id');
-        if(top>=offset && top < offset + height) {
-            links.forEach(link => {
-                link.classList.remove('active');
-                document.querySelector('nav a[href*=' + id + ']').classList.add('active');
-            })
-        }
-    })
-};
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  //Animate Links
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+
+  //Hamburger Animation
+  hamburger.classList.toggle("toggle");
+});
